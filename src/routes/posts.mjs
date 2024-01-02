1401b3919bd97a754a0513712ce988f5551d44bc
FChatelot <florianchatelot@gmail.com>
2024-01-02 12:57:31 +0100

    qsfd

--- src/routes/posts.mjs
+++ src/routes/posts.mjs
@@ -1,19 +1,19 @@
 import express from "express";
 import {ObjectId} from "mongodb";
-import db from "../db/conn.mjs";
+import getDb from "../db/conn.mjs";
 
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  let collection = await db.collection("posts");
+  let collection = await getDb.collection("posts");
   let results = await collection.find({}).toArray();
   res.send(results).status(200);
 });
 
 // This section will help you get a single post by id
 router.get("/:id", async (req, res) => {
-  let collection = await db.collection("posts");
+  let collection = await getDb.collection("posts");
   let query = {_id: new ObjectId(req.params.id)};
   let result = await collection.findOne(query);
 
@@ -27,7 +27,7 @@ router.post("/", async (req, res) => {
     title: req.body.title,
     content: req.body.content,
   };
-  let collection = await db.collection("posts");
+  let collection = await getDb.collection("posts");
   let result = await collection.insertOne(newDocument);
   res.send(result).status(204);
 });
@@ -42,7 +42,7 @@ router.patch("/:id", async (req, res) => {
     }
   };
 
-  let collection = await db.collection("posts");
+  let collection = await getDb.collection("posts");
   let result = await collection.updateOne(query, updates);
   res.send(result).status(200);
 });
@@ -51,7 +51,7 @@ router.patch("/:id", async (req, res) => {
 router.delete("/:id", async (req, res) => {
   const query = { _id: new ObjectId(req.params.id) };
 
-  const collection = db.collection("posts");
+  const collection = getDb.collection("posts");
   let result = await collection.deleteOne(query);
 
   res.send(result).status(200);
