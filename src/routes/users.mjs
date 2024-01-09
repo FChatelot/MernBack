a4df0e9bea711856760c48feacc093a5956eca4f
FChatelot <florianchatelot@gmail.com>
2024-01-09 17:04:41 +0100

    startin v2

---
+++ src/routes/users.mjs
@@ -0,0 +1,7 @@
+//https://github.com/salarc123/classius
+//https://dev.to/salarc123/how-to-connect-a-react-frontend-with-a-nodejs-express-backend-50i9
+
+//https://github.com/bradtraversy/mern-auth
+//https://www.youtube.com/watch?v=R4AhvYORZRY
+
+//https://www.mongodb.com/developer/products/mongodb/mongodb-schema-design-best-practices/
