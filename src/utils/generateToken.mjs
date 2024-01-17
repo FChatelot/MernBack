c4933ee1b5b072b5fab5ecf6098c5405ae4a7037
FChatelot <florianchatelot@gmail.com>
2024-01-17 15:10:16 +0100

    test

--- src/utils/generateToken.mjs
+++ src/utils/generateToken.mjs
@@ -6,8 +6,8 @@ const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     });
     res.cookie("jwt", token, {
         httpOnly:true,
-        secure: process.env.NODE_ENV !== "developpement",
-        sameSite:"strict",
+        secure: true,//process.env.NODE_ENV !== "developpement"
+        sameSite:"none",//"strict",
         maxAge: 1*24*60*60*1000 //en gros 1 journée
     })
 }
